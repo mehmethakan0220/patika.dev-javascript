3ae7f10ec0b7eb886b39549226f2c96d5c51e1b7
mehmethakan0220 <mehmethakan0220@gmail.com>
2022-03-03 18:56:44 +0300

    ejs template engine used

--- 08.pcat.project/app.js
+++ 08.pcat.project/app.js
@@ -1,26 +1,37 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const ejs = require('ejs');
 const port = 3000;
 
 const app = express();
 
 
 app.use(cors());
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.set('view engine','ejs');
 
 app.get("/",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+    // res.sendFile(path.resolve(__dirname,'temp/index.html'))
+    res.render('index')
 })
-app.get("/index.html",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+app.get("/index",(req,res)=>{
+    // res.sendFile(path.resolve(__dirname,'temp/index.html'))
+    res.render('index')
 })
-app.get("/about.html",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'temp/about.html'))
+app.get("/abou",(req,res)=>{
+    // res.sendFile(path.resolve(__dirname,'temp/about.html'))
+    res.render('about')
 })
-app.get("/contact.html",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'temp/contact.html'))
+app.get("/add",(req,res)=>{
+    // res.sendFile(path.resolve(__dirname,'temp/contact.html'))
+    res.render('add')
+})
+
+app.get("/videos",(req,res)=>{
+    // res.sendFile(path.resolve(__dirname,'temp/contact.html'))
+    res.render('video')
 })
 
 
-app.listen(port,console.log(`server ${port}'unda başlatıldı`));
+app.listen(port,console.log(`server ${port}'unda başlatildi`));
